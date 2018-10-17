c9cb83ea4980a647e1b067d755d19a0c36691f8e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-17 13:55:21 -0700

    Spiral

--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,17 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
+		Robot saitama = new Robot();
 		
-		// 5. Set your robot's pen down 
-		
-		// 3. Set the robot to go at max speed (100)
+		saitama.setSpeed(100);
 		
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+
 			// 7. Change the pen color to random
-		
+		saitama.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+			saitama.move();
 			// 2. Turn the robot 360/7 degrees to the right
-		
-			// 8. Set the pen width to i
-			
+			saitama.turn(360/7);
 	}
 }
